3a13cb1ccc11290d16fdd0fd8edea33dc2815cc2
Hemanvitha Pullela <hemanvithapullela@gmail.com>
2024-11-21 23:01:58 +0530

    first commit

---
+++ functions/api.js
@@ -0,0 +1,18 @@
+import express from "express";
+import ServerlessHttp from "serverless-http";
+
+const app = express(); 
+
+app.get('/.netlify/functions/api',(req,res)=>{
+    return res.json({
+        messages :"hello world!"
+    })
+})
+
+const handler = ServerlessHttp(app);
+
+
+module.exports.handler = async(event,context) =>{
+    const result = await handler(event, context);  // Pass event and context to the handler
+    return result;
+}
